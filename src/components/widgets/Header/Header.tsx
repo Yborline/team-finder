5a424d9ec2563178dc1a420ee682ad83ef611388
Yaroslav Borys <borrline@gmail.com>
2024-08-25 22:05:11 +0300

    add modal in the header

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import { useTranslation } from "react-i18next";
 import LanguageButton from "@components/shared/LanguageButton/LanguageButton";
+import { useState } from "react";
+import Modal from "@components/shared/Modal/Modal";
 // import ThemeToggle from "@components/shared/ThemeToggle/ThemeToggle";
 
 export const Header = () => {
   const { t } = useTranslation();
+  const [toggleModal, setToggleModal] = useState(false);
+  const handleCloseModal = () => {
+    setToggleModal(!toggleModal);
+  };
 
   return (
     <header className={styles.header}>
@@ -17,10 +23,20 @@ export const Header = () => {
           <LanguageButton />
         </div>
         <div className={styles.boxRight}>
-          <button className={styles.auth}>{t("header.login")}</button>
-          <button className={styles.auth}>{t("header.signUp")}</button>
+          <button onClick={handleCloseModal} className={styles.auth}>
+            {t("header.login")}
+          </button>
+          <button onClick={handleCloseModal} className={styles.auth}>
+            {t("header.signUp")}
+          </button>
         </div>
       </div>
+
+      {toggleModal && (
+        <Modal close={handleCloseModal}>
+          <div>ahahaha</div>
+        </Modal>
+      )}
     </header>
   );
 };
