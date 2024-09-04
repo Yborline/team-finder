353f090129ffdfe4bfe0c1bc6461ca22e89c663a
Yaroslav Borys <borrline@gmail.com>
2024-09-04 22:40:46 +0300

    add component ModalBackdropAuth

---
+++ src/components/shared/Modal/ModalBackdropAuth/ModalBackdropAuth.tsx
@@ -0,0 +1,23 @@
+import React, { FC, ReactNode } from "react";
+import styles from "./ModalBackdropAuth.module.scss";
+import { IconContext } from "react-icons";
+import { IoCloseSharp } from "react-icons/io5";
+
+interface IPropsModalAuth {
+  close: () => void;
+  children: ReactNode;
+}
+
+const ModalBackdropAuth: FC<IPropsModalAuth> = ({ close, children }) => {
+  return (
+    <div className={styles.boxAuth}>
+      <IconContext.Provider value={{ className: styles.closeIcon }}>
+        <IoCloseSharp onClick={close} />
+      </IconContext.Provider>
+
+      {children}
+    </div>
+  );
+};
+
+export default ModalBackdropAuth;
