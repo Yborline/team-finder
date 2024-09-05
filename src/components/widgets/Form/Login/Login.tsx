8375629ad1ca0524d0d73379f96d169a0449cfb0
Yaroslav Borys <borrline@gmail.com>
2024-09-05 21:26:29 +0300

    changed logic and styles component Login

--- src/components/widgets/Form/Login/Login.tsx
+++ src/components/widgets/Form/Login/Login.tsx
@@ -1,18 +1,25 @@
+import ButtonAuth from "@components/shared/Button/ButtonAuth/ButtonAuth";
 import ErrorText from "@components/shared/ErrorText/ErrorText";
 import InputModal from "@components/shared/InputModal/InputModal";
 import ModalBackdropAuth from "@components/shared/Modal/ModalBackdropAuth/ModalBackdropAuth";
 import { yupResolver } from "@hookform/resolvers/yup";
-import React from "react";
+import styles from "./Login.module.scss";
 import { SubmitHandler, useForm } from "react-hook-form";
-import schemaLogin from "validations/login";
-import schemaRegister from "validations/register";
+import schemaLogin from "@validations/login";
+import { FC } from "react";
+import Google from "@components/shared/Button/ButtonOtherAuth/Google/Google";
+import Discord from "@components/shared/Button/ButtonOtherAuth/Discord/Discord";
+import AuthOtherBox from "@components/widgets/AuthOtherBox/AuthOtherBox";
 
-const Login = () => {
-  interface IFormLogin {
-    name: string;
-    password: string;
-  }
+interface IPropsLogin {
+  close: () => void;
+}
 
+interface IFormLogin {
+  name: string;
+  password: string;
+}
+const Login: FC<IPropsLogin> = ({ close }) => {
   const {
     register,
     handleSubmit,
@@ -22,23 +29,14 @@ const Login = () => {
 
   return (
     <ModalBackdropAuth close={close}>
-      <form className={styles.boxRegister} onSubmit={handleSubmit(onSubmit)}>
+      <form className={styles.boxLogin} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.boxInputs}>
           <InputModal
             hookForm={register("name", { required: true, maxLength: 20 })}
             text="Нікнейм"
             aria-invalid={errors.name ? "true" : "false"}
           />
-
           <ErrorText error={errors.name} />
-
-          <InputModal
-            hookForm={register("email", { required: true, maxLength: 20 })}
-            text="Електронна адреса"
-            aria-invalid={errors.name ? "true" : "false"}
-          />
-          <ErrorText error={errors.email} />
-          {/* {errors.email && <p role="alert">{errors.email?.message}</p>} */}
           <InputModal
             hookForm={register("password", { required: true, maxLength: 20 })}
             text="Пароль"
@@ -47,22 +45,9 @@ const Login = () => {
             repeatPassword={true}
           />
           <ErrorText error={errors.password} />
-          {/* {errors.password && <p role="alert">{errors.password?.message}</p>} */}
-          <InputModal
-            hookForm={register("repeatPassword", {
-              required: true,
-              maxLength: 20,
-            })}
-            text="Підтвердження паролю"
-            type="password"
-            aria-invalid={errors.repeatPassword ? "true" : "false"}
-            repeatPassword={true}
-          />
-          <ErrorText error={errors.repeatPassword} />
         </div>
-        <button type="submit" className={styles.btnRegister}>
-          Зареєструватися
-        </button>
+        <ButtonAuth text="Увійти" type="submit" />
+        <AuthOtherBox />
       </form>
     </ModalBackdropAuth>
   );
