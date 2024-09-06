add119988f61392fb6d9043088f7aaa039d268ce
Yaroslav Borys <borrline@gmail.com>
2024-09-06 20:27:10 +0300

    added root reducer and changed store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer";
 
-export const store = configureStore({
-  reducer: {},
+import logger from "redux-logger";
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export default store;
