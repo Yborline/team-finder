cc9cda3551456312a07f1ad2a81db47c1473a88d
Yaroslav Borys <borrline@gmail.com>
2024-09-06 19:52:32 +0300

    added provier in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 import { Suspense } from "react";
+import { store } from "@redux/store";
+import { Provider } from "react-redux";
 // import { useTranslation } from "react-i18next";
 
 function App() {
   // const { t, i18n } = useTranslation();
   return (
-    <Suspense fallback="...loading">
-      <RouterProvider router={router} />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback="...loading">
+        <RouterProvider router={router} />
+      </Suspense>
+    </Provider>
   );
 }
 
