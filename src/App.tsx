e2d30cd02a84b81993d43617e77097cb8acecf40
Yaroslav Borys <borrline@gmail.com>
2024-09-09 23:46:03 +0300

    fixed import in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 import { Suspense } from "react";
-import { store } from "@redux/store";
+import store from "@redux/store";
 import { Provider } from "react-redux";
 // import { useTranslation } from "react-i18next";
 
