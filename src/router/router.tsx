5e482f6e96d6fc82859ad19a3110f28985f666fc
Yaroslav Borys <borrline@gmail.com>
2024-09-10 23:08:43 +0300

    added router (teamList)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/Home/Home";
+import TeamList from "@pages/TeamList/TeamList";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/team",
+        element: <TeamList />,
       },
     ],
   },
