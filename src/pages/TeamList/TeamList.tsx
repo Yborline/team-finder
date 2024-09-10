1218f029bf963ed7a60b7bd3ce691b8e46b212ae
Yaroslav Borys <borrline@gmail.com>
2024-09-10 23:08:22 +0300

    added mokks and structure teamList

--- src/pages/TeamList/TeamList.tsx
+++ src/pages/TeamList/TeamList.tsx
@@ -1,5 +1,35 @@
+import ListPlayers from "@components/widgets/ListPlayers/ListPlayers";
+
+const mokkaPlayers = [
+  {
+    name: "Lol",
+    type: "solo",
+    game: "dota 2",
+    tags: ["mid", "pudge", "4k mmr"],
+    id: "11112111",
+  },
+  {
+    name: "Lol",
+    type: "solo",
+    game: "dota 2",
+    tags: ["mid", "pudge", "4k mmr"],
+    id: "1231321321",
+  },
+  {
+    name: "Lol",
+    type: "solo",
+    game: "dota 2",
+    tags: ["mid", "pudge", "4k mmr"],
+    id: "1231321322",
+  },
+];
+
 const TeamList = () => {
-  return;
+  return (
+    <>
+      <ListPlayers list={mokkaPlayers} />
+    </>
+  );
 };
 
 export default TeamList;
