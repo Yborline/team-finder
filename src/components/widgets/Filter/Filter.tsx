82bf8f3bddba32741111c9a5a40b95ad115bdc5c
Yaroslav Borys <borrline@gmail.com>
2024-09-11 18:50:04 +0300

    added modal Filter

--- src/components/widgets/Filter/Filter.tsx
+++ src/components/widgets/Filter/Filter.tsx
@@ -2,8 +2,18 @@ import { IconContext } from "react-icons";
 import styles from "./Filter.module.scss";
 import { BsSearch } from "react-icons/bs";
 import { IoFilterSharp } from "react-icons/io5";
+import { useState } from "react";
+import FilterForm from "../Form/FilterForm/FilterForm";
+import ModalBackdropAuth from "@components/shared/Modal/ModalBackdropAuth/ModalBackdropAuth";
+import Modal from "@components/shared/Modal/Modal";
 
 const Filter = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div className={styles.boxFilter}>
       <div className={styles.boxInput}>
@@ -12,12 +22,17 @@ const Filter = () => {
           <BsSearch />
         </IconContext.Provider>
       </div>
-      <button className={styles.buttonFilter}>
+      <button onClick={toggleModal} className={styles.buttonFilter}>
         Filter
         <IconContext.Provider value={{ className: styles.iconFilter }}>
           <IoFilterSharp />
         </IconContext.Provider>
       </button>
+      {showModal && (
+        <Modal close={toggleModal}>
+          <FilterForm />
+        </Modal>
+      )}
     </div>
   );
 };
