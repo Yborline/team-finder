cbf6c83b1f498bf0c02275eb4e9d33420ab22aeb
Yaroslav Borys <borrline@gmail.com>
2024-09-11 12:14:46 +0300

    changed structure in the ListPlayers

--- src/components/widgets/ListPlayers/ListPlayers.tsx
+++ src/components/widgets/ListPlayers/ListPlayers.tsx
@@ -1,15 +1,21 @@
 import { IListPlayerItem } from "@interfaces/teamList";
 import { FC } from "react";
+import styles from "./ListPlayers.module.scss";
 
 const ListPlayers: FC<{ list: IListPlayerItem[] }> = ({ list }) => {
   return (
-    <ul>
+    <ul className={styles.listPlayers}>
       {list.map(({ name, tags, type, game, id }) => (
-        <li key={id}>
-          <h3>{name}</h3>
-          <p>{game}</p>
-          <p>{type}</p>
-          {tags && tags.map((tag) => <p key={tag}>{tag}</p>)}
+        <li className={styles.boxPlayer} key={id}>
+          <h3 className={styles.namePlayer}>{name}</h3>
+          <p>game: {game}</p>
+          <p>type: {type}</p>
+          {tags && (
+            <>
+              <p>tags: </p>
+              <p>{tags.join(", ")}</p>
+            </>
+          )}
         </li>
       ))}
     </ul>
