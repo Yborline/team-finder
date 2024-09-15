534194f5e1b3dd259a12d5a288eecb382b460ba7
Yaroslav Borys <borrline@gmail.com>
2024-09-15 20:54:15 +0300

    changet logic with cookie

--- src/redux/auth/auth-operations.tsx
+++ src/redux/auth/auth-operations.tsx
@@ -1,6 +1,7 @@
+import { IFormLogin, IFormRegisterSend } from "@interfaces/form";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import api from "@api/api";
-import axios from "axios";
+import api from "@api/api";
+// import axios from "axios";
 
 const token = {
   set(token: string) {
@@ -10,13 +11,13 @@ const token = {
     document.cookie = "token=; Max-Age=0; path=/; secure; SameSite=Strict";
   },
 };
-axios.defaults.baseURL = "http://95.135.51.126/api/";
+// axios.defaults.baseURL = "http://95.135.51.126/api/";
 
 const register = createAsyncThunk(
   "auth/register",
-  async (credentials, thunkAPI) => {
+  async (credentials: IFormRegisterSend, thunkAPI) => {
     try {
-      const { data } = await axios.post("users/register", credentials);
+      const { data } = await api.post("users/register", credentials);
       token.set(data.token);
       return data;
     } catch (error) {
@@ -25,19 +26,23 @@ const register = createAsyncThunk(
   }
 );
 
-const logIn = createAsyncThunk("auth/login", async (credentials, thunkAPI) => {
-  try {
-    const { data } = await axios.post("users/login", credentials);
-    token.set(data.token);
-    return data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
+const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials: IFormLogin, thunkAPI) => {
+    try {
+      const { data } = await api.post("users/auth", credentials);
+
+      token.set(data.access_token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
-});
+);
 
 const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
-    await axios.post("users/logout");
+    await api.post("users/logout");
     token.unset();
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
@@ -54,7 +59,7 @@ const fetchCurrentUser = createAsyncThunk(
     // }
     // token.set(persistedToken);
     try {
-      const { data } = await axios.get("/users/current");
+      const { data } = await api.get("/users/current");
       console.log(data);
       return data;
     } catch (error) {
