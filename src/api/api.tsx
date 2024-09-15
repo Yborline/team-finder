ac9c80101588fd73131038b4b0c3b37639d7ab77
Yaroslav Borys <borrline@gmail.com>
2024-09-15 20:41:16 +0300

    changed api , console withCredentials

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,10 +3,9 @@ import axios, { AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 // Створення кастомного інстансу Axios
 const api = axios.create({
   baseURL: "http://95.135.51.126/api/",
-  withCredentials: true, // Дозволяє відправляти кукі з кожним запитом
+  // withCredentials: true,
 });
 
-// Інтерцептор для додавання токену до заголовків
 api.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
     const token = document.cookie
