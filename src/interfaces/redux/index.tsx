131d777da3dba96d5e59cf68567e4ecf9ec35760
Yaroslav Borys <borrline@gmail.com>
2024-09-16 21:05:30 +0300

    changed typer RootState (fixed)

--- src/interfaces/redux/index.tsx
+++ src/interfaces/redux/index.tsx
@@ -1,7 +1,8 @@
 import store from "@redux/store";
-import { rootReducer } from "@redux/rootReducer";
 import { useDispatch } from "react-redux";
 
-export type RootState = ReturnType<typeof rootReducer>;
+// export type RootState = ReturnType<typeof rootReducer>;
+export type IRootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
