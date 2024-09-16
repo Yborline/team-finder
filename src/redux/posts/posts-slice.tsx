7bac2caecb4b0080f7c72165fc0daf8679bf28b5
Yaroslav Borys <borrline@gmail.com>
2024-09-16 19:46:45 +0300

    created post-slicer and aded start logic

---
+++ src/redux/posts/posts-slice.tsx
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import postsOperations from "./posts-operations";
+import { IErrorResponse } from "@interfaces/slicer";
+import { PostsState, Post } from "@interfaces/posts";
+
+const initialState = {
+  posts: [] as PostsState[],
+  loading: false,
+  error: false,
+  notify: null as string | null,
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(postsOperations.getPosts.fulfilled, (state, { payload }) => {
+        console.log(payload);
+        state.posts = payload.posts;
+        state.loading = false;
+      })
+      .addCase(postsOperations.getPosts.pending, (state, { payload }) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(postsOperations.getPosts.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = true;
+        state.notify = (payload as IErrorResponse).response.data;
+      })
+      .addCase(
+        postsOperations.addPost.fulfilled,
+        (state, { payload }: PayloadAction<{ post: Post }>) => {
+          state.posts = [payload.post, ...state.posts];
+        }
+      );
+  },
+});
+
+export default postsSlice;
