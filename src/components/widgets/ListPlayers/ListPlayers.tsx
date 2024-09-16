b0c8d68a7c889b7db742b1be0cb2423accdde1dc
Yaroslav Borys <borrline@gmail.com>
2024-09-16 19:49:42 +0300

    added redirect in the post ( ListPlayer)

--- src/components/widgets/ListPlayers/ListPlayers.tsx
+++ src/components/widgets/ListPlayers/ListPlayers.tsx
@@ -1,12 +1,23 @@
 import { IListPlayerItem } from "@interfaces/teamList";
 import { FC } from "react";
 import styles from "./ListPlayers.module.scss";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ListPlayers: FC<{ list: IListPlayerItem[] }> = ({ list }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const redirectToPlayerPage = (id) => {
+    const currentPath = location.pathname;
+    navigate(`${currentPath}/${id}`);
+  };
   return (
     <ul className={styles.listPlayers}>
       {list.map(({ name, tags, type, game, id }) => (
-        <li className={styles.boxPlayer} key={id}>
+        <li
+          onClick={() => redirectToPlayerPage(id)}
+          className={styles.boxPlayer}
+          key={id}
+        >
           <h3 className={styles.namePlayer}>{name}</h3>
           <p>game: {game}</p>
           <p>type: {type}</p>
