f6ca74522ec2f72e50479285c4c61d9fa328ac17
Yaroslav Borys <borrline@gmail.com>
2024-09-16 21:03:31 +0300

    added new path (page Post)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/Home/Home";
 import TeamList from "@pages/TeamList/TeamList";
+import Post from "@pages/TeamList/Post/Post";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/team",
         element: <TeamList />,
+      },
+      {
+        path: "/team/:id",
+        element: <Post />, // Отдельная страница Post, но с общим родителем по пути
       },
     ],
   },
