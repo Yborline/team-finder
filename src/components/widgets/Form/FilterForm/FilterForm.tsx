495532f7293591ae114ba5abb7792626607895df
Yaroslav Borys <borrline@gmail.com>
2024-09-18 21:06:05 +0300

    changed component FilterForm

--- src/components/widgets/Form/FilterForm/FilterForm.tsx
+++ src/components/widgets/Form/FilterForm/FilterForm.tsx
@@ -40,23 +40,34 @@ const FilterForm: FC<FilterFormProps> = ({ showModal }) => {
             className={styles.FilterFormBox}
             onSubmit={handleSubmit(onSubmit)}
           >
-            <div>
-              <label htmlFor="search">Search:</label>
-              <input
-                id="search"
-                type="text"
-                {...register("search", { required: "This field is required" })}
-              />
-              {errors.search && <span>{errors.search.message}</span>}
+            <div className={styles.boxOneFilter}>
+              <label htmlFor="category">Type:</label>
+              <select id="category" {...register("category")}>
+                <option value="all">all</option>
+                <option value="solo">solo</option>
+                <option value="party">party</option>
+              </select>
+            </div>
+            <div className={styles.boxOneFilter}>
+              <label htmlFor="category">Sort Date:</label>
+              <select id="category" {...register("category")}>
+                <option value="newDate">New Date</option>
+                <option value="oldDate">Old Date</option>
+              </select>
+            </div>
+            <div className={styles.boxOneFilter}>
+              <label htmlFor="category">Connection:</label>
+              <select id="category" {...register("category")}>
+                <option value="newDate">Discord</option>
+                <option value="oldDate">Gmail</option>
+                <option value="oldDate">Telegram</option>
+                <option value="oldDate">viber</option>
+              </select>
             </div>
 
             <div>
-              <label htmlFor="category">Category:</label>
-              <select id="category" {...register("category")}>
-                <option value="all">All</option>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-              </select>
+              <label htmlFor="isActive">Active:</label>
+              <input id="isActive" type="checkbox" {...register("isActive")} />
             </div>
 
             <div>
