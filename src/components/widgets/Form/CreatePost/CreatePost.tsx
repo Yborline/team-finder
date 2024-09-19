7d64a39d734c243e41f482046cc2e7acb26e4cfa
Yaroslav Borys <borrline@gmail.com>
2024-09-19 22:02:52 +0300

    create component CreatePost

---
+++ src/components/widgets/Form/CreatePost/CreatePost.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CreatePost.module.scss";
+
+const CreatePost = () => {
+  return <div className={styles.FormCreatePost}></div>;
+};
+
+export default CreatePost;
