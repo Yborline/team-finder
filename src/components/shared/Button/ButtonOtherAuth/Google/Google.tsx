f30cc527d5c17e05e868bc5fc618f41af66ec60f
Yaroslav Borys <borrline@gmail.com>
2024-09-19 22:02:31 +0300

    added logic to click in google

--- src/components/shared/Button/ButtonOtherAuth/Google/Google.tsx
+++ src/components/shared/Button/ButtonOtherAuth/Google/Google.tsx
@@ -3,10 +3,20 @@ import { ButtonDiscordGoogle } from "@interfaces/form/index";
 import ButtonOtherAuth from "../ButtonOtherAuth";
 import google from "@assets/img/auth/google.png";
 import styles from "./Google.module.scss";
+import { useDispatch } from "react-redux";
+import operationsAuth from "@redux/auth/auth-operations";
+import { AppDispatch, useAppDispatch } from "@interfaces/redux";
+import { postGoogleAuth } from "@api/fetchGoogle";
 
-const Google: FC<ButtonDiscordGoogle> = ({ onClick, text }) => {
+const Google: FC<ButtonDiscordGoogle> = ({ text }) => {
+  // const dispatch = useAppDispatch<AppDispatch>();
+
+  const handleCLickGoogle = async () => {
+    const response = await postGoogleAuth("/auth/google");
+    window.open(response, "_blank");
+  };
   return (
-    <ButtonOtherAuth onClick={onClick} text={text}>
+    <ButtonOtherAuth onClick={handleCLickGoogle} text={text}>
       <img className={styles.google} src={google} />
     </ButtonOtherAuth>
   );
