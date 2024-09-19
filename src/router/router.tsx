093fda529adac93d849b43b1c1d00e91f2092353
Yaroslav Borys <borrline@gmail.com>
2024-09-19 22:05:14 +0300

    added router create

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,8 @@ import { Layout } from "../layout/Layout";
 import { Home } from "../pages/Home/Home";
 import TeamList from "@pages/TeamList/TeamList";
 import Post from "@pages/TeamList/Post/Post";
+import Create from "@pages/Create/Create";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
       {
         path: "/team/:id",
         element: <Post />, // Отдельная страница Post, но с общим родителем по пути
+      },
+      {
+        path: "/create",
+        element: (
+          <PrivateRoute>
+            <Create />
+          </PrivateRoute>
+        ),
       },
     ],
   },
