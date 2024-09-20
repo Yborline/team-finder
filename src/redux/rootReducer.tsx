53d9f2951c076b78d3f366fcd4026da23a758ec3
Yaroslav Borys <borrline@gmail.com>
2024-09-20 20:59:09 +0300

    added reducer Modal

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth-slice";
-import posts from "./posts/posts-operations";
+import posts from "./posts/posts-slice";
+import modalReducer from "./modal/modal-slice";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   posts: posts,
+  modal: modalReducer,
 });
