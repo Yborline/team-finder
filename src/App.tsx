38dd372a7a890bd22433c3e762694b85c2f188f1
Yaroslav Borys <borrline@gmail.com>
2024-09-20 20:54:56 +0300

    added Google provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import { router } from "./router/router";
 import { Suspense } from "react";
 import store from "@redux/store";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 // import { useTranslation } from "react-i18next";
 
 function App() {
   // const { t, i18n } = useTranslation();
+  const googleId = import.meta.env.VITE_GOOGLE_CLIENTID!;
+
+  // const googleId = `384010096834-b2nqf1gfe13v90nfiglkqcpgd0a73deh.apps.googleusercontent.com`;
   return (
     <Provider store={store}>
-      <Suspense fallback="...loading">
-        <RouterProvider router={router} />
-      </Suspense>
+      <GoogleOAuthProvider clientId={googleId}>
+        <Suspense fallback="...loading">
+          <RouterProvider router={router} />
+        </Suspense>
+      </GoogleOAuthProvider>
     </Provider>
   );
 }
