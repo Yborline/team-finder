6224fd8bbb432a393725bc8bab3711bc963ec0eb
Yaroslav Borys <borrline@gmail.com>
2024-09-21 19:54:39 +0300

    added start logic Form createPost

--- src/components/widgets/Form/CreatePost/CreatePost.tsx
+++ src/components/widgets/Form/CreatePost/CreatePost.tsx
@@ -1,8 +1,50 @@
 import React from "react";
 import styles from "./CreatePost.module.scss";
+import { useForm } from "react-hook-form";
+import { IFormCreatePost } from "@interfaces/form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import schemaCreatePost from "@validations/createPost";
+import InputModal from "@components/shared/InputModal/InputModal";
+import TextArea from "@components/shared/Input/TextArea/TextArea";
 
 const CreatePost = () => {
-  return <div className={styles.FormCreatePost}></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormCreatePost>({ resolver: yupResolver(schemaCreatePost) });
+
+  return (
+    <form className={styles.FormCreatePost}>
+      <div>
+        <InputModal
+          classN={"transparent"}
+          text="Гра"
+          hookForm={register("game")}
+        />
+
+        <InputModal
+          classN={"transparent"}
+          text="Гра"
+          hookForm={register("game")}
+        />
+        <TextArea
+          placeholder={"Ваше повідомлення"}
+          hookForm={register("comment")}
+        />
+        <div>
+          <label>
+            <input type="radio" value="solo" {...register("type")} />
+            Solo
+          </label>
+          <label>
+            <input type="radio" value="group" {...register("type")} />
+            Group
+          </label>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default CreatePost;
