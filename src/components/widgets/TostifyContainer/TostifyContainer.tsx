667c9bbf14882e2fafc6398ae23d4b5ceff2353e
Yaroslav Borys <borrline@gmail.com>
2024-09-22 20:08:16 +0300

    added Component TostifyContainer

---
+++ src/components/widgets/TostifyContainer/TostifyContainer.tsx
@@ -0,0 +1,20 @@
+import { ToastContainer, Flip } from "react-toastify";
+const TostifyContainer = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Flip}
+    />
+  );
+};
+
+export default TostifyContainer;
