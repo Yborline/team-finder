3fe0460b8093a122c977f005b13ee962e5e37994
Yaroslav Borys <borrline@gmail.com>
2024-09-23 19:34:01 +0300

    added new component SocialsButton

---
+++ src/components/shared/Button/SocialsButton/SocialsButton.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import styles from "./SocialsButton.module.scss";
+
+interface ISocialsButton {
+  text: string;
+  img: string;
+  onClick: () => void;
+}
+
+const SocialsButton: FC<ISocialsButton> = ({ text, img, onClick }) => {
+  return (
+    <button type="button" className={styles.buttonSocials} onClick={onClick}>
+      <img className={styles.imgSocials} src={img} /> {text}
+    </button>
+  );
+};
+
+export default SocialsButton;
