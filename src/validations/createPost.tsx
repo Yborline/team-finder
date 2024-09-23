3f8be01b7b6de9614a2869f65af9db3bd5c7590f
Yaroslav Borys <borrline@gmail.com>
2024-09-23 19:26:24 +0300

    changed validation CreatePost

--- src/validations/createPost.tsx
+++ src/validations/createPost.tsx
@@ -1,10 +1,27 @@
 import * as yup from "yup";
 
-const schemaCreatePost = yup.object({
-  type: yup.string().oneOf(["solo", "group"]).required("Тип обов'язковий!"),
-  comment: yup.string().min(3, "Текс повинен бути не менше 3 символів!"),
-  tags: yup.array().of(yup.string()),
-  game: yup.string().required("Напишіть назву гри!"),
-});
+const schemaCreatePost = yup
+  .object({
+    type: yup
+      .string()
+      .oneOf(["lookingForPlayers", "lookingForGroup"])
+      .required("Тип обов'язковий!"),
+    comment: yup
+      .string()
+      .nullable()
+      .min(3, "Текст повинен бути не менше 3 символів!"),
+    // tags: yup.array().of(yup.string()),
+    game: yup.string().required("Напишіть назву гри!"),
+    discord: yup.string().required("Напишіть назву гри!"),
+    telegram: yup.string().required("Напишіть назву гри!"),
+  })
+  .test(
+    "discordOrTelegram",
+    "Заповніть хоча б одне поле: Discord або Telegram",
+    function (value) {
+      const { discord, telegram } = value;
+      return !!discord || !!telegram;
+    }
+  );
 
 export default schemaCreatePost;
