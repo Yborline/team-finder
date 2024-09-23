0ee0f9bad42d26869b62bcde2b4b4f38f542a3ae
Yaroslav Borys <borrline@gmail.com>
2024-09-23 19:32:49 +0300

    added new component AnimatedButton and added styles

---
+++ src/components/shared/Button/AnimatedButton/AnimatedButton.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import styles from "./AnimatedButton.module.scss";
+import { typeB } from "@interfaces/button";
+
+interface IPropsRegularButton {
+  type?: typeB;
+  text: string;
+  onClick?: () => void;
+}
+
+const AnimatedButton: FC<IPropsRegularButton> = ({
+  type = "button",
+  text,
+  onClick,
+}) => {
+  return (
+    <button type={type} onClick={onClick} className={styles.btn}>
+      <strong>{text}</strong>
+      <div className={styles.containerStars}>
+        <div className={styles.stars}></div>
+      </div>
+      <div className={styles.glow}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+      </div>
+    </button>
+  );
+};
+
+export default AnimatedButton;
