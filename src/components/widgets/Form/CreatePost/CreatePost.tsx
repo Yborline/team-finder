3832c70b2d33dc1d37c083d286d7a742f5f8cee4
Yaroslav Borys <borrline@gmail.com>
2024-09-23 19:31:27 +0300

    added logic in the CreatePost and added styles

--- src/components/widgets/Form/CreatePost/CreatePost.tsx
+++ src/components/widgets/Form/CreatePost/CreatePost.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./CreatePost.module.scss";
-import { useForm, useFormContext } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { IFormCreatePost } from "@interfaces/form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import schemaCreatePost from "@validations/createPost";
@@ -9,44 +9,129 @@ import TextArea from "@components/shared/Input/TextArea/TextArea";
 import Radio from "@components/shared/Input/Radio/Radio";
 import InputForTags from "@components/shared/Input/InputModal/InputForTags";
 import { notify } from "@components/widgets/Tostify/Tostify";
+import AnimatedButton from "@components/shared/Button/AnimatedButton/AnimatedButton";
+import ErrorText from "@components/shared/ErrorText/ErrorText";
+import SocialsButton from "@components/shared/Button/SocialsButton/SocialsButton";
+import imgDiscord from "@assets/img/auth/discord.png";
+import imgTelegram from "@assets/img/socials/telegram.png";
+
+const maxTegs = 10;
 
 const CreatePost = () => {
   const [tags, setTags] = useState<string[]>([]);
+  const [inputCurrent, setInputCurrent] = useState("");
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm<IFormCreatePost>({ resolver: yupResolver(schemaCreatePost) });
+  } = useForm<IFormCreatePost>({
+    resolver: yupResolver(schemaCreatePost),
+    defaultValues: {
+      game: "",
+      comment: null,
+      type: "lookingForPlayers",
+      discord: "",
+      telegram: "",
+    },
+  });
 
   const handlehandleSaveTags = (value: string) => {
-    notify("success", "успішно");
+    if (tags.length >= maxTegs) {
+      notify("error", `Максимальна кількість тегів - ${maxTegs}шт`);
+      return;
+    }
     setTags((prevState) => [...prevState, value]);
   };
 
-  return (
-    <form className={styles.FormCreatePost}>
-      <div>
-        <InputModal
-          classN={"transparent"}
-          text="Гра"
-          hookForm={register("game")}
-        />
+  const handleSubmitCreatePost: SubmitHandler<IFormCreatePost> = (values) => {
+    console.log(values);
+    console.log(tags);
+    reset();
+    notify("success", "Ваш пост відправлено");
+  };
+  const handleOpenSocials = (value: string) => {
+    if (value === inputCurrent) setInputCurrent("");
+    else setInputCurrent(value);
+  };
+  console.log(inputCurrent);
 
-        <TextArea
-          placeholder={"Ваше повідомлення"}
-          hookForm={register("comment")}
+  return (
+    <form
+      onSubmit={handleSubmit(handleSubmitCreatePost)}
+      className={styles.FormCreatePost}
+    >
+      <InputModal
+        classN={"transparent"}
+        text="Гра"
+        hookForm={register("game")}
+      />
+      <ErrorText error={errors.game} />
+
+      <TextArea
+        placeholder={"Ваше повідомлення"}
+        hookForm={register("comment")}
+      />
+      <ErrorText error={errors.comment} />
+      <div className={styles.boxRadio}>
+        <Radio
+          value="lookingForPlayers"
+          hookForm={register("type")}
+          text="Пошук гравця"
+        />{" "}
+        <Radio
+          value="lookingForGroup"
+          hookForm={register("type")}
+          text="Пошук команди"
+        />{" "}
+      </div>
+      <ErrorText error={errors.type} />
+      <div className={styles.boxSocials}>
+        <SocialsButton
+          onClick={() => handleOpenSocials("telegram")}
+          text="Telegram"
+          img={imgTelegram}
         />
-        <div className={styles.boxRadio}>
-          <Radio value="solo" hookForm={register("type")} text="Solo" />{" "}
-          <Radio value="group" hookForm={register("type")} text="Group" />{" "}
-        </div>
-        <InputForTags
-          classN={"transparent"}
-          placeholder="Введіть теги для пошуку"
-          handleSaveWord={handlehandleSaveTags}
+        <SocialsButton
+          onClick={() => handleOpenSocials("discord")}
+          text="Discord"
+          img={imgDiscord}
         />
       </div>
-      <p>{tags.join(",")}</p>
+      {inputCurrent === "telegram" && (
+        <>
+          <InputModal
+            classN={"transparent"}
+            text="Посилання на телеграм"
+            hookForm={register("telegram")}
+          />
+          <ErrorText error={errors.telegram} />
+        </>
+      )}
+      {inputCurrent === "discord" && (
+        <>
+          <InputModal
+            classN={"transparent"}
+            text="Посилання на діскорд"
+            hookForm={register("discord")}
+          />
+          <ErrorText error={errors.discord} />
+        </>
+      )}
+
+      <InputForTags
+        classN={"transparent"}
+        placeholder="Введіть теги для пошуку"
+        handleSaveWord={handlehandleSaveTags}
+      />
+      {tags.length > 0 && (
+        <>
+          <p className={styles.descTags}>Ваші додані теги:</p>
+          <p className={styles.listTags}>{tags.join(", ")}</p>
+        </>
+      )}
+
+      <AnimatedButton type="submit" text="Відправити" />
     </form>
   );
 };
