cf69fad4f8ae0252651e31cc7082370adf97ca62
Yaroslav Borys <borrline@gmail.com>
2024-10-03 21:54:24 +0300

    changed logic filterForm

--- src/components/widgets/Form/FilterForm/FilterForm.tsx
+++ src/components/widgets/Form/FilterForm/FilterForm.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react";
-import { useForm } from "react-hook-form";
 import styles from "./FilterForm.module.scss";
 import { AnimatePresence, motion } from "framer-motion";
+import { useAppDispatch } from "@interfaces/redux";
+import { setFilter } from "@redux/posts/posts-slice";
+import { FilterField } from "@interfaces/posts";
+import { useSelector } from "react-redux";
+import {
+  getFilterDate,
+  getFilterSocials,
+  getFilterType,
+} from "@redux/posts/posts-selector";
 
 interface FilterForm {
   search: string;
@@ -14,15 +22,16 @@ interface FilterFormProps {
 }
 
 const FilterForm: FC<FilterFormProps> = ({ showModal }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FilterForm>();
+  const filterType = useSelector(getFilterType);
+  const filterDate = useSelector(getFilterDate);
+  const filterSocials = useSelector(getFilterSocials);
 
-  const onSubmit = (data: FilterForm) => {
-    console.log(data);
-    // Здесь можно обработать данные фильтра
+  const dispatch = useAppDispatch();
+
+  const handleChangeFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(event.target.id);
+    const { id, value } = event.target;
+    dispatch(setFilter({ field: id as FilterField, value }));
   };
 
   return (
@@ -30,49 +39,47 @@ const FilterForm: FC<FilterFormProps> = ({ showModal }) => {
       {showModal && (
         <motion.div
           key="filterForm"
-          className={styles.boxForm}
+          className={styles.boxFormFilter}
           initial={{ left: "-100%" }} // Початкове положення за межами екрана зліва
           animate={{ left: "0%" }} // Кінцеве положення на екрані
           exit={{ left: "-100%" }}
           transition={{ duration: 0.3 }}
         >
-          <form
-            className={styles.FilterFormBox}
-            onSubmit={handleSubmit(onSubmit)}
-          >
+          <form className={styles.FilterFormBox}>
             <div className={styles.boxOneFilter}>
-              <label htmlFor="category">Type:</label>
-              <select id="category" {...register("category")}>
+              <label htmlFor="type">Type:</label>
+              <select
+                value={filterType}
+                onChange={handleChangeFilter}
+                id="type"
+              >
+                <option value="all">Всі картки</option>
+                <option value="lookingForPlayers">Пошук гравця</option>
+                <option value="lookingForGroup">Пошук команди</option>
+              </select>
+            </div>
+            <div className={styles.boxOneFilter}>
+              <label htmlFor="date">Sort Date:</label>
+              <select
+                value={filterDate}
+                onChange={handleChangeFilter}
+                id="date"
+              >
+                <option value="new">New Date</option>
+                <option value="old">Old Date</option>
+              </select>
+            </div>
+            <div className={styles.boxOneFilter}>
+              <label htmlFor="socials">Connection:</label>
+              <select
+                value={filterSocials}
+                onChange={handleChangeFilter}
+                id="socials"
+              >
                 <option value="all">all</option>
-                <option value="solo">solo</option>
-                <option value="party">party</option>
+                <option value="discord">Discord</option>
+                <option value="telegram">Telegram</option>
               </select>
-            </div>
-            <div className={styles.boxOneFilter}>
-              <label htmlFor="category">Sort Date:</label>
-              <select id="category" {...register("category")}>
-                <option value="newDate">New Date</option>
-                <option value="oldDate">Old Date</option>
-              </select>
-            </div>
-            <div className={styles.boxOneFilter}>
-              <label htmlFor="category">Connection:</label>
-              <select id="category" {...register("category")}>
-                <option value="newDate">Discord</option>
-                <option value="oldDate">Gmail</option>
-                <option value="oldDate">Telegram</option>
-                <option value="oldDate">viber</option>
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="isActive">Active:</label>
-              <input id="isActive" type="checkbox" {...register("isActive")} />
-            </div>
-
-            <div>
-              <label htmlFor="isActive">Active:</label>
-              <input id="isActive" type="checkbox" {...register("isActive")} />
             </div>
 
             <button type="submit">Apply Filter</button>
