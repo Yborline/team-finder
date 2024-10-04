a9d4518681ee27ae253a807326fdfb8b491ab6a7
Yaroslav Borys <borrline@gmail.com>
2024-10-04 22:05:15 +0300

    delete mokkies in the Page  and added selector and added checks posts list

--- src/pages/TeamList/TeamList.tsx
+++ src/pages/TeamList/TeamList.tsx
@@ -1,89 +1,28 @@
 import Filter from "@components/widgets/Filter/Filter";
 import ListPlayers from "@components/widgets/ListPlayers/ListPlayers";
-import { IListPlayerItem } from "@interfaces/teamList";
 import styles from "./TeamList.module.scss";
-import { useDebugValue, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { AppDispatch, useAppDispatch } from "@interfaces/redux";
 import postsOperations from "@redux/posts/posts-operations";
-import { getPosts } from "@redux/posts/posts-selector";
-
-const mokkaPlayers: IListPlayerItem[] = [
-  {
-    name: "LLLLLLLLLLLLLLLLLLLL",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "11112111",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "123132132",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "12313213",
-  },
-  {
-    name: "LLLLLLLLLLLLLLLLLLLL",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "1111211",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "12313213212",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "12313213223",
-  },
-  {
-    name: "LLLLLLLLLLLLLLLLLLLL",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "1111211141",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "12313213215",
-  },
-  {
-    name: "Lol",
-    type: "solo",
-    game: "dota 2",
-    tags: ["mid", "pudge", "4k mmr"],
-    id: "12313213226",
-  },
-];
+import { getSelectFilteredPosts } from "@redux/posts/posts-selector";
+// import { resetFilter } from "@redux/posts/posts-slice";
 
 const TeamList = () => {
   const dispatch = useAppDispatch<AppDispatch>();
-  const posts = useSelector(getPosts);
+  const posts = useSelector(getSelectFilteredPosts);
   useEffect(() => {
     dispatch(postsOperations.getPosts());
-  }, []);
+
+    // return () => {
+    //   dispatch(resetFilter());
+    // };
+  }, [dispatch]);
+
   return (
     <div className={styles.boxTeam}>
       <Filter />
-      <ListPlayers list={mokkaPlayers} />
+      {posts && posts.length > 0 && <ListPlayers list={posts} />}
     </div>
   );
 };
