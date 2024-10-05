38c8eeee258061ee2503e62d772bdc1dc530bb04
Yaroslav Borys <borrline@gmail.com>
2024-10-05 21:11:29 +0300

    added dispatch current user

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,8 +3,17 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../components/widgets/Header/Header";
 import { Footer } from "../components/widgets/Footer/Footer";
 import TostifyContainer from "@components/widgets/TostifyContainer/TostifyContainer";
+import { useEffect } from "react";
+import { useAppDispatch } from "@interfaces/redux";
+import operationsAuth from "@redux/auth/auth-operations";
 
 export const Layout = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(operationsAuth.fetchCurrentUser());
+  });
+
   return (
     <div className={styles.projectBox}>
       <Header />
