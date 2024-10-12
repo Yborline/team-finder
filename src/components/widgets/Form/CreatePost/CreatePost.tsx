a2be509138859d9420b22f685ff3e5844cf9bebf
Yaroslav Borys <borrline@gmail.com>
2024-10-12 22:55:45 +0300

    added new input title

--- src/components/widgets/Form/CreatePost/CreatePost.tsx
+++ src/components/widgets/Form/CreatePost/CreatePost.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./CreatePost.module.scss";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { IFormCreatePost } from "@interfaces/form";
+import { IFormCreatePost, SocialKeys } from "@interfaces/form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import schemaCreatePost from "@validations/createPost";
 import InputModal from "@components/shared/Input/InputModal/InputModal";
@@ -14,27 +14,41 @@ import ErrorText from "@components/shared/ErrorText/ErrorText";
 import SocialsButton from "@components/shared/Button/SocialsButton/SocialsButton";
 import imgDiscord from "@assets/img/auth/discord.png";
 import imgTelegram from "@assets/img/socials/telegram.png";
+import { useAppDispatch } from "@interfaces/redux";
+import postsOperations from "@redux/posts/posts-operations";
 
 const maxTegs = 10;
 
 const CreatePost = () => {
   const [tags, setTags] = useState<string[]>([]);
-  const [inputCurrent, setInputCurrent] = useState("");
+  const [inputCurrent, setInputCurrent] = useState<SocialKeys | "">("");
+  const dispatch = useAppDispatch();
   const {
     register,
     handleSubmit,
     reset,
+
     formState: { errors },
   } = useForm<IFormCreatePost>({
     resolver: yupResolver(schemaCreatePost),
     defaultValues: {
+      title: "",
       game: "",
       comment: null,
       type: "lookingForPlayers",
-      discord: "",
-      telegram: "",
+      socials: {
+        discord: null,
+        telegram: null,
+      },
     },
   });
+
+  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { value } = e.target;
+  //   const name = e.target.name as FieldNames;
+  //   clearErrors("discordOrTelegram"); // Очистить ошибку при вводе
+  //   setValue(name, value ? value : null); // Установить новое значение
+  // };
 
   const handlehandleSaveTags = (value: string) => {
     if (tags.length >= maxTegs) {
@@ -45,12 +59,13 @@ const CreatePost = () => {
   };
 
   const handleSubmitCreatePost: SubmitHandler<IFormCreatePost> = (values) => {
-    console.log(values);
-    console.log(tags);
+    const newObj = { ...values, tags };
+    console.log(newObj);
+    dispatch(postsOperations.addPost(newObj));
+    setTags([]);
     reset();
-    notify("success", "Ваш пост відправлено");
   };
-  const handleOpenSocials = (value: string) => {
+  const handleOpenSocials = (value: SocialKeys) => {
     if (value === inputCurrent) setInputCurrent("");
     else setInputCurrent(value);
   };
@@ -63,11 +78,16 @@ const CreatePost = () => {
     >
       <InputModal
         classN={"transparent"}
+        text="Заголовок"
+        hookForm={register("title")}
+      />
+      <ErrorText error={errors.title} />
+      <InputModal
+        classN={"transparent"}
         text="Гра"
         hookForm={register("game")}
       />
       <ErrorText error={errors.game} />
-
       <TextArea
         placeholder={"Ваше повідомлення"}
         hookForm={register("comment")}
@@ -101,24 +121,34 @@ const CreatePost = () => {
       {inputCurrent === "telegram" && (
         <>
           <InputModal
+            // onChange={handleInputChange}
             classN={"transparent"}
-            text="Посилання на телеграм"
-            hookForm={register("telegram")}
+            text="Ваш нікнейм в телеграмі"
+            hookForm={register("socials.telegram")}
           />
-          <ErrorText error={errors.telegram} />
+          {/* <ErrorText error={errors.socials?.telegram} /> */}
         </>
       )}
       {inputCurrent === "discord" && (
         <>
           <InputModal
+            // onChange={handleInputChange}
             classN={"transparent"}
-            text="Посилання на діскорд"
-            hookForm={register("discord")}
+            text="Ваш ID Discord"
+            hookForm={register("socials.discord")}
+            type="text"
           />
-          <ErrorText error={errors.discord} />
+          {/* <ErrorText error={errors.socials?.discord} /> */}
         </>
       )}
 
+      {/* {
+        (!discordValue || !telegramValue) && inputCurrent === "" ? (
+          <ErrorText error={errors.socials?.discord} />
+        ) : inputCurrent && errors.socials?.[inputCurrent] ? (
+          <ErrorText error={errors.socials[inputCurrent]} />
+        ) : null // Or handle the case when there's no error for the inputCurrent
+      } */}
       <InputForTags
         classN={"transparent"}
         placeholder="Введіть теги для пошуку"
@@ -130,7 +160,6 @@ const CreatePost = () => {
           <p className={styles.listTags}>{tags.join(", ")}</p>
         </>
       )}
-
       <AnimatedButton type="submit" text="Відправити" />
     </form>
   );
