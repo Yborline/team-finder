62a1d88cb8f955e95a7e6c657dfb6e3b673e6590
Yaroslav Borys <borrline@gmail.com>
2024-10-18 23:45:05 +0300

    added Link in the header - buttonUser

--- src/components/shared/Button/ButtonUser/ButtonUser.tsx
+++ src/components/shared/Button/ButtonUser/ButtonUser.tsx
@@ -1,14 +1,17 @@
 import { FaUserCircle } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import styles from "./ButtonUser.module.scss";
+import { Link } from "react-router-dom";
 
 const ButtonUser = () => {
   // const nameUser = useSelector(getUserName);
 
   return (
-    <IconContext.Provider value={{ className: styles.userButton }}>
-      <FaUserCircle />
-    </IconContext.Provider>
+    <Link to="/user">
+      <IconContext.Provider value={{ className: styles.userButton }}>
+        <FaUserCircle />
+      </IconContext.Provider>
+    </Link>
   );
 };
 
