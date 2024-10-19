01f33617ea2b32f07d5539a1f76bc4efddb476e6
Yaroslav Borys <borrline@gmail.com>
2024-10-19 22:52:35 +0300

    added getUser in auth selector

--- src/redux/auth/auth-selector.tsx
+++ src/redux/auth/auth-selector.tsx
@@ -3,3 +3,4 @@ import { IRootState } from "@interfaces/redux";
 export const getUserName = (state: IRootState) => state.auth.user.name;
 export const getLoggedIn = (state: IRootState) => state.auth.isLoggedIn;
 export const getLoading = (state: IRootState) => state.auth.isLoading;
+export const getUser = (state: IRootState) => state.auth.user;
