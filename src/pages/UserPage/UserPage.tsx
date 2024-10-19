b945a162c42ee06c79a0eb7f78c2633987f63804
Yaroslav Borys <borrline@gmail.com>
2024-10-19 22:53:05 +0300

    changed UserPage

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -3,41 +3,60 @@ import styles from "./UserPage.module.scss";
 import { useAppDispatch } from "@interfaces/redux";
 import operationsAuth from "@redux/auth/auth-operations";
 import ButtonStandart from "@components/shared/Button/ButtonStandart/ButtonStandart";
-import InputModal from "@components/shared/Input/InputModal/InputModal";
+import UserForm from "@components/widgets/Form/UserForm/UserForm";
+import { useSelector } from "react-redux";
+import { getUser } from "@redux/auth/auth-selector";
+import { IUserForm } from "@interfaces/user/user";
 
 const objNew = {
   name: "user",
   email: "email",
-  telegram: "telegram",
-  discord: "discord",
+  telegramLink: "telegram",
+  discordUsername: "discord",
 };
 
 const UserPage = () => {
-  const { name, email, telegram, discord } = objNew;
+  const { id, name, email, telegramLink, discordUsername } = objNew;
   const [showChange, setShowChange] = useState(false);
+  const user = useSelector(getUser);
   const dispatch = useAppDispatch();
 
-  const startChangeUser = () => {
-    if (!showChange) setShowChange(!showChange);
-    else dispatch(operationsAuth.changeInfoUser());
+  const changeStatusChange = () => {
+    setShowChange(!showChange);
+  };
+
+  const handleSaveInfo = (info: IUserForm) => {
+    dispatch(operationsAuth.changeInfoUser(info));
   };
 
   return (
     <div className={styles.boxUser}>
-      <div>
-        <p>Ім'я: {name}</p>
-      </div>
-      <div>
-        <p>Пошта: {email}</p>
-      </div>
-      <div>
-        <p>Телеграм: {telegram}</p>
-      </div>
-      <div>
-        <p>Діскорд: {discord}</p>
-      </div>
-
-      <ButtonStandart onClick={startChangeUser}>Змінити данні</ButtonStandart>
+      {showChange ? (
+        <div className={styles.boxInfoForm}>
+          <UserForm user={user} onSubmit={handleSaveInfo} />
+          <ButtonStandart onClick={changeStatusChange}>
+            Відмінити
+          </ButtonStandart>
+        </div>
+      ) : (
+        <div className={styles.boxInfo}>
+          <div>
+            <p>Ім'я: {name}</p>
+          </div>
+          <div>
+            <p>Пошта: {email}</p>
+          </div>
+          <div>
+            <p>Телеграм: {telegramLink}</p>
+          </div>
+          <div>
+            <p>Діскорд: {discordUsername}</p>
+          </div>
+          <ButtonStandart onClick={changeStatusChange}>
+            Змінити данні
+          </ButtonStandart>
+        </div>
+      )}
     </div>
   );
 };
