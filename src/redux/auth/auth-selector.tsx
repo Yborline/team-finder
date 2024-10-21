faf5214aa425ccc26aab35a047774ee0fce4670f
Yaroslav Borys <borrline@gmail.com>
2024-10-21 22:22:46 +0300

    added new selectors error and notify

--- src/redux/auth/auth-selector.tsx
+++ src/redux/auth/auth-selector.tsx
@@ -4,3 +4,5 @@ export const getUserName = (state: IRootState) => state.auth.user.name;
 export const getLoggedIn = (state: IRootState) => state.auth.isLoggedIn;
 export const getLoading = (state: IRootState) => state.auth.isLoading;
 export const getUser = (state: IRootState) => state.auth.user;
+export const getErrorUser = (state: IRootState) => state.auth.error;
+export const getNotifyUser = (state: IRootState) => state.auth.notify;
