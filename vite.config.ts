1600ae1f6427bdd718fb27e87e3790e8a296dc3f
Yaroslav Borys <borrline@gmail.com>
2024-12-22 16:59:55 +0300

    changed configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,34 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+          // Разделение ленивых маршрутов (по примеру)
+          if (id.includes("/pages/")) {
+            return "pages";
+          }
+        },
+      },
+    },
+  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ["react", "react-dom", "react-router-dom"],
+  //       },
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
@@ -16,6 +44,7 @@ export default defineConfig({
       "@validations": path.resolve(__dirname, "./src/validations"),
       "@redux": path.resolve(__dirname, "./src/redux"),
       "@api": path.resolve(__dirname, "./src/api"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 });
