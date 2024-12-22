e12d8881a0bf9c2696c23d280c342b47777f6aa6
Yaroslav Borys <borrline@gmail.com>
2024-12-22 16:56:15 +0300

    memo ButtonExit

--- src/components/shared/Button/ButtonExit/ButtonExit.tsx
+++ src/components/shared/Button/ButtonExit/ButtonExit.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { IconContext } from "react-icons";
 import { ImExit } from "react-icons/im";
 import styles from "./ButtonExit.module.scss";
@@ -7,12 +7,12 @@ interface IButtonExit {
   onClick: () => void;
 }
 
-const ButtonExit: FC<IButtonExit> = ({ onClick }) => {
+const ButtonExit: FC<IButtonExit> = memo(({ onClick }) => {
   return (
     <IconContext.Provider value={{ className: styles.exitIcon }}>
       <ImExit onClick={onClick} />
     </IconContext.Provider>
   );
-};
+});
 
 export default ButtonExit;
