3c2181f74e808aa891e2f2858a5f423f95e3893c
Yaroslav Borys <borrline@gmail.com>
2024-12-22 16:57:37 +0300

    Update Layout.tsx
    changed import in layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,13 @@
 import styles from "./Layout.module.scss";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/widgets/Header/Header";
-import { Footer } from "../components/widgets/Footer/Footer";
+import Header from "../components/widgets/Header/Header";
+import Footer from "../components/widgets/Footer/Footer";
 import TostifyContainer from "@components/widgets/TostifyContainer/TostifyContainer";
 import { useEffect } from "react";
 import { useAppDispatch } from "@interfaces/redux";
 import operationsAuth from "@redux/auth/auth-operations";
 
-export const Layout = () => {
+const Layout = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -23,3 +23,5 @@ export const Layout = () => {
     </div>
   );
 };
+
+export default Layout;
